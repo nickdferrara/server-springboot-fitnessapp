f001ded8376b7cd3904c90e9740154254adef091
Nicholas Ferrara <nickdferrara@gmail.com>
2024-02-12 18:45:35 -0500

    Make initial commmit

---
+++ src/main/kotlin/com/nickdferrara/serverspringbootfitnessapp/ServerSpringbootFitnessappApplication.kt
@@ -0,0 +1,11 @@
+package com.nickdferrara.serverspringbootfitnessapp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServerSpringbootFitnessappApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServerSpringbootFitnessappApplication>(*args)
+}
